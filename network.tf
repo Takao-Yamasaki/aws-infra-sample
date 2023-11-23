3a9c81cff6010662130a5044d7a0779565f9275d
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-11-23 18:50:02 +0900

    add:security

--- network.tf
+++ network.tf
@@ -58,15 +58,16 @@ resource "aws_eip" "sample-nat_gateway" {
 resource "aws_nat_gateway" "sample-ngw-01" {
   allocation_id = aws_eip.sample-nat_gateway.id
   subnet_id = aws_subnet.sample-subnet-public01.id
+  # 暗黙的な依存関係
   depends_on = [ aws_internet_gateway.sample-igw ]
 }
 
-# ルートテーブル
+# パブリックルートテーブル
 resource "aws_route_table" "sample-rt-public" {
   vpc_id = aws_vpc.sample-vpc.id
 }
 
-# ルート（デフォルトルートの設定）
+# パブリックルート（デフォルトルートの設定）
 # IGW経由でInternetへデータを流す
 resource "aws_route" "sample-public" {
   route_table_id = aws_route_table.sample-rt-public.id
@@ -74,16 +75,63 @@ resource "aws_route" "sample-public" {
   destination_cidr_block = "0.0.0.0/0"
 }
 
-# ルートテーブルの関連付け
+# パブリックルートテーブルの関連付け
 resource "aws_route_table_association" "sample-public01" {
+  subnet_id = aws_subnet.sample-subnet-public01.id
+  route_table_id = aws_route.sample-public.id
+}
+
+# パブリックルートテーブルの関連付け
+resource "aws_route_table_association" "sample-public02" {
+  subnet_id = aws_subnet.sample-subnet-public02.id
+  route_table_id = aws_route.sample-public.id
+}
+
+# プライベートルートテーブル
+resource "aws_route_table" "sample-rt-private01" {
+  vpc_id = aws_vpc.sample-vpc.id
+}
+
+# プライベートルートテーブルの関連付け
+resource "aws_route_table_association" "sample-private01" {
   subnet_id = aws_subnet.sample-subnet-private01.id
   route_table_id = aws_route.sample-public.id
 }
 
-# ルートテーブルの関連付け
-resource "aws_route_table_association" "sample-public02" {
-  subnet_id = aws_subnet.sample-subnet-private02.id
-  route_table_id = aws_route.sample-public.id
+# プライベートルートテーブルのルート（デフォルトルートの設定）
+# NAT経由でInternetへデータを流す
+resource "aws_route" "sample-private01" {
+  route_table_id = aws_route_table.sample-rt-public.id
+  gateway_id = aws_nat_gateway.sample-ngw-01.id
+  destination_cidr_block = "0.0.0.0/0"
 }
 
-# TODO: プライベートルートテーブルの作成から
+# TODO: sample-rt-private02はNATを配置していないので、ルートテーブルを設置していない
+
+# セキュリティグループの定義(踏み台サーバー用)
+resource "aws_security_group" "sample_sg_bastion" {
+  name = "sample_sg_bastion"
+  description = "for bastion server"
+  vpc_id = aws_vpc.sample-vpc.id
+}
+
+# セキュリティグループルール（インバウンド）
+resource "aws_security_group_rule" "ingress_sample_sg_bastion" {
+  type = "ingress"
+  from_port = "22"
+  to_port = "22"
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sample_sg_bastion.id
+}
+
+# セキュリティグループルール（アウトバウンド）
+# 全ての通信を許可
+resource "aws_security_group_rule" "egress_sample_sg_bastion" {
+  type = "egress"
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sample_sg_bastion.id
+}
